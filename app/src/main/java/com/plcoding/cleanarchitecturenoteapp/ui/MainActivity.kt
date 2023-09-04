ed6c729998fec843581ff7a5cad1b190ef787237
Yustar <36198000+yuzutaru@users.noreply.github.com>
2023-09-04 07:45:45 +0700

    load screen in main activity

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/ui/MainActivity.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/ui/MainActivity.kt
@@ -8,6 +8,14 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.add_edit_note.AddEditNoteScreen
+import com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes.NotesScreen
+import com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.util.Screen
 import com.plcoding.cleanarchitecturenoteapp.ui.theme.CleanArchitectureNoteAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,7 +23,39 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CleanArchitectureNoteAppTheme {
-                
+                Surface(
+                    color = MaterialTheme.colors.background
+                ) {
+                    val navController = rememberNavController()
+
+                    NavHost(navController = navController, startDestination = Screen.NotesScreen.route
+                    ) {
+                        composable(route = Screen.NotesScreen.route) {
+                            NotesScreen(navController = navController)
+                        }
+                        composable(
+                            route = Screen.AddEditNoteScreen.route + "?noteId={noteId}&noteColor={noteColor}",
+                            arguments = listOf(
+                                navArgument(
+                                    name = "noteId"
+                                ) {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                },
+                                navArgument(
+                                    name = "noteColor"
+                                ) {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                },
+                            )
+                        ) {
+                            val color = it.arguments?.getInt("noteColor") ?: -1
+                            AddEditNoteScreen(navController = navController,
+                                noteColor = color)
+                        }
+                    }
+                }
             }
         }
     }
